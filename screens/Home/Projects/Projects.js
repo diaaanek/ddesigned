8fcbf76d98cc5765fb709f5d2dd72a5b0e6d1175
“Angel <angelCL0@outlook.com>
2022-06-05 14:02:37 -0400

    Fucking Amazing!!

--- screens/Home/Projects/Projects.js
+++ screens/Home/Projects/Projects.js
@@ -3,25 +3,45 @@ import Tag from "../../../components/Tag";
 import TextOverlap from "../../../components/TextOverlap";
 import Project from "./Project";
 import styles from "./projects.module.css";
+import React, { useState } from "react";
+import VideoModal from "../../../components/VideoModal";
 
 const projects = [
   {
-    image: "/images/01.jpg",
-    title: "Wasting Angels",
-  },
-  {
+    embed: "WVWxPIzEM3A",
     image: "/images/01.jpg",
     title: "Reputation",
   },
   {
-    image: "/images/01.jpg",
-    title: "Lemon Tree",
+    embed: "WABOrIYhR94",
+    image: "/images/02.jpg",
+    title: "Copped Up",
+  },
+  {
+    embed: "JXxAnZaZrG0",
+    image: "/images/03.jpg",
+    title: "Wrapped Around Your Finger",
+  },
+  {
+    embed: "_a0T5qwxANg",
+    image: "/images/04.jpg",
+    title: "I Like You",
+  },
+  {
+    embed: "o3vqfdvatsQ",
+    image: "/images/05.jpg",
+    title: "I Cannot Be",
+  },
+  {
+    embed: "HdtJtIQhzbQ",
+    image: "/images/06.jpg",
+    title: "Insane",
   },
 ];
 
 const Projects = () => {
   return (
-    <div className={cn("section")}>
+    <div className={cn("section section-pb")}>
       <div className={cn("container", styles.container)}>
         <div className={styles.upper_content}>
           <div className={styles.content}>
