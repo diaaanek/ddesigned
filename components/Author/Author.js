fa0cd34ac00794dbd020a767448638200c37fe32
“Angel <angelCL0@outlook.com>
2022-06-05 23:29:38 -0400

    fixed

--- components/Author/Author.js
+++ components/Author/Author.js
@@ -6,7 +6,12 @@ const Author = () => {
   return (
     <a href="https://uriostegui.vercel.app" target="_blank" rel="noreferrer">
       <div className={styles.container}>
-        <Icon name="tag" fillRule="evenodd" clipRule="evenodd" />
+        <Icon
+          className={styles.icon}
+          name="tag"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        />
         <p className={cn("caption-2-bold", styles.title)}>Purchase $30</p>
       </div>
     </a>
