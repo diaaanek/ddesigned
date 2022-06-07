095c47c148769d0e83a30a9c88b18e3420494ace
“Angel <angelCL0@outlook.com>
2022-06-07 00:14:06 -0400

    flde

--- screens/Services/Pricing/Pricing.js
+++ screens/Services/Pricing/Pricing.js
@@ -90,9 +90,7 @@ const Pricing = () => {
             <TextOverlap title="Pricing" text="Pricing" />
           </div>
 
-          <button className={cn("button-small", styles.button)}>
-            View Services
-          </button>
+          <button className={cn("button-small", styles.button)}>Support</button>
         </div>
 
         <div className={styles.wrapper}>
