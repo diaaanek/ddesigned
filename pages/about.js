eebdf42f85a7dc55a9f194966e855bbad8c10d8b
“Angel <angelCL0@outlook.com>
2022-06-09 00:37:34 -0400

    CTA

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,19 @@
 import cn from "classnames";
 import Layout from "../components/Layout";
 import { Hero, Testimonials } from "../screens/About";
+import CTA from "../components/CTA";
 
 const About = () => {
   return (
     <Layout>
       <Hero />
       <Testimonials />
+      <CTA
+        number="02"
+        background="#92A5EF"
+        title="30% Discount"
+        button="Learn More"
+      />
     </Layout>
   );
 };
