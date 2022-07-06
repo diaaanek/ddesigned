47539bc59d329a5fe33a30d7c8b47371676f62be
diaaanek <dkorongy@gmail.com>
2022-07-06 11:26:49 -0400

    tag

--- components/Layout.js
+++ components/Layout.js
@@ -10,18 +10,11 @@ const Layout = ({ children }) => {
       <Head>
         <script
           dangerouslySetInnerHTML={{
-            __html: `async
-      src="https://www.googletagmanager.com/gtag/js?id=UA-139835856-1"
-    ></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-
-      gtag("config", "UA-139835856-1");
-   `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M7K8MSB');`,
           }}
         />
 
@@ -57,6 +50,14 @@ const Layout = ({ children }) => {
       </Head>
       <Header />
       {children}
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7K8MSB"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      />
+
       {/* <Footer /> */}
       <FooterTip />
       {/* <Author /> */}
