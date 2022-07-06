ebd3b8d837b4c59f69a3cff7f02ee8af56efe993
diaaanek <dkorongy@gmail.com>
2022-07-06 11:30:34 -0400

    tag

--- components/Layout.js
+++ components/Layout.js
@@ -18,6 +18,22 @@ const Layout = ({ children }) => {
           }}
         />
 
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-139835856-1"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+      
+            gtag("config", "UA-139835856-1");`,
+          }}
+        />
+
         <link
           rel='preload'
           href='/fonts/AirbnbCereal-Book.ttf'
